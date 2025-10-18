812776cb0af2716f1e68e7f92b074e38417887b7
Nejc Mihelcic <nejc.mihelcic8@gmail.com>
2025-10-18 20:47:53 +0100

    povrnil originalni testni primer mreze

--- dn-1.ml
+++ dn-1.ml
@@ -453,17 +453,15 @@ type resitev = int array array
  Na primer, zgornjo mrežo in rešitev bi predstavili s seznamoma:
 [*----------------------------------------------------------------------------*)
 
-let primer_mreze : mreza = [|
- [|None; None; None; Some 9; None; Some 2; Some 7; Some 3; Some 8|];
- [|None; Some 6; None; None; None; Some 5; None; None; Some 9|];
- [|Some 8; Some 2; Some 9; Some 3; None; None; Some 6; None; Some 1|];
- [|None; None; Some 1; None; None; Some 6; None; Some 8; Some 2|];
- [|None; Some 9; Some 3; None; Some 1; None; Some 4; None; Some 5|];
- [|None; Some 8; None; Some 7; None; None; Some 9; Some 1; None|];
- [|Some 9; None; None; None; None; Some 7; None; None; None|];
- [|None; Some 3; Some 8; Some 6; None; None; Some 5; None; None|];
- [|None; None; None; Some 5; None; Some 1; Some 8; Some 9; Some 4|]
-|]
+let primer_mreze : mreza = [|[|Some 5; Some 4; None; None; Some 7; None; None; None; None|];
+    [|Some 6; None; None; Some 1; Some 9; Some 5; None; None; None|];
+    [|None; Some 9; Some 8; None; None; None; None; Some 6; None|];
+    [|Some 8; None; None; None; Some 6; None; None; None; Some 3|];
+    [|Some 4; None; None; Some 8; None; Some 3; None; None; Some 1|];
+    [|Some 7; None; None; None; Some 2; None; None; None; Some 6|];
+    [|None; Some 6; None; None; None; Some 7; Some 8; None; None|];
+    [|None; None; None; Some 4; Some 1; Some 9; None; None; Some 5|];
+    [|None; None; None; None; Some 8; None; None; Some 7; Some 9|]|]
 
 let primer_resitve : resitev = [|
   [|5; 4; 3; 6; 7; 8; 9; 1; 2|];
